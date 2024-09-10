920f722dc6b6e7a7b9bdcc00caaa465e3d504d05
Darek Mazur <dev@darekmazur.it>
2024-09-10 08:19:47 +0200

    Handle POST

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 import express from "express";
 
-const data = {
+interface itemTypes {
+  id: string;
+  body: string;
+  isFinished: boolean;
+  createdAt: Date;
+}
+
+interface dataTypes {
+  items: itemTypes[];
+}
+
+const data: dataTypes = {
   items: [],
 };
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.use(express.json());
+
 app.get("/api", (_req, res) => {
   res.send(data);
+});
+
+app.post("/api/post", (req, res) => {
+  data.items.push(req.body);
+  res.status(200).end();
 });
 
 app.listen(port, () => {
