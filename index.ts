bb2d75d73da1d85a7da424c30190de1017ca5466
Darek Mazur <dev@darekmazur.it>
2024-09-10 18:39:52 +0200

    Handle DELETE item

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
-interface itemTypes {
+interface IitemTypes {
   id: string;
   body: string;
   isFinished: boolean;
-  createdAt: Date;
+  createdAt: number;
 }
 
-interface dataTypes {
-  items: itemTypes[];
+interface IdataTypes {
+  items: IitemTypes[];
 }
 
-const data: dataTypes = {
+const data: IdataTypes = {
   items: [],
 };
 
@@ -25,9 +26,32 @@ app.get("/api", (_req, res) => {
 });
 
 app.post("/api", (req, res) => {
-  data.items.push(req.body);
-  res.send(req.body);
+  const item: IitemTypes = {
+    id: uuidv4(),
+    body: req.body.body,
+    isFinished: req.body.isFinished,
+    createdAt: Date.now(),
+  };
+
+  data.items.push(item);
+  res.send(item);
   res.status(200).end();
+});
+
+app.delete("/api/:itemId", (req, res) => {
+  const itemId = req.params.itemId;
+
+  if (data.items.find((item) => item.id === itemId)) {
+    const filteredItems = data.items.filter((item) => item.id !== itemId);
+
+    data.items = [...filteredItems];
+
+    res.send("item deleted");
+    res.status(200).end();
+  } else {
+    res.send(`Item with ID ${itemId} does not exist`);
+    res.status(404).end();
+  }
 });
 
 app.listen(port, () => {
